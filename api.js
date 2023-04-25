40b10a866e56856c078a9713a6dda46b4abb3bef
AldehilS <sanheraldehil@outlook.com>
2023-04-25 00:07:27 -0600

    45toeji5

--- api.js
+++ api.js
@@ -1,25 +1,20 @@
 const express = require('express');
+const mysql = require('mysql')
 
 const app = express();
 const port = 8080;
-const ipAddr = 'localhost';
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+'/public'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req,res) => {
     res.render('index')
 })
 
-// app.post('login/:user/:pass')
+app.get('/login/:user/:pass', (req,res) => {
 
-app.listen(port,()=>{console.log('funciona!!!')})
+})
 
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
-//HUMBERTO SE LA TRAGA ENTERA
+app.listen(port,() => {
+    console.log('funciona!!!')
+})
