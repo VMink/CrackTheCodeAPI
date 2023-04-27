1d197819cf5355e7440a07d8baa0b81683b59420
AldehilS <sanheraldehil@outlook.com>
2023-04-27 16:13:13 -0600

    dapowijdesoaie

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mssql = require('mssql');
 const { createHash } = require('crypto');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = 8080;
@@ -36,6 +37,7 @@ connectDb();
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.render('index')
