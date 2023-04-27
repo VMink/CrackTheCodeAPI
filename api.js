f993c2579517a7a61eda57a310cebfba01d96c02
AldehilS <sanheraldehil@outlook.com>
2023-04-27 11:31:57 -0600

    adpjapwd

--- api.js
+++ api.js
@@ -35,7 +35,7 @@ connectDb();
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+'/public'))
-app.use(express.json);
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.render('index')
