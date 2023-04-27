ce9e79a3da0a36f0ede1fba8defe3882315a52d0
AldehilS <sanheraldehil@outlook.com>
2023-04-27 16:31:29 -0600

    awdpojsefio

--- public/js/post.js
+++ public/js/post.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
+    console.log(formData)
     fetch("http://52.55.120.19:8080/register", {
         method: 'POST',
         body: formData
