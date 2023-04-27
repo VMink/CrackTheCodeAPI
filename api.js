9a8dcf57a081f41b083bb0aa5ada83ef0f79f729
AldehilS <sanheraldehil@outlook.com>
2023-04-27 13:12:19 -0600

    dawodi

--- api.js
+++ api.js
@@ -53,7 +53,7 @@ app.get('/login/:user/:pass', async (req,res) => {
 
     const request = mssql.Request(dbConfig);
     request.input('user',user);
-    const rows = (await mssql.query`select idUsuario,contraseña from usuario where idUsuario = @user`).recordset;
+    const rows = (await request.query`select idUsuario,contraseña from usuario where idUsuario = @user`).recordset;
 
     const user_data = rows[0];
 
