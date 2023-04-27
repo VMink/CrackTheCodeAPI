04747ae32befbec79b76738028fc69f33ef55910
AldehilS <sanheraldehil@outlook.com>
2023-04-27 16:25:05 -0600

    adfosiefsefij

---
+++ public/js/post.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    fetch("http://52.55.120.19:8080/register", {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(result => {
+        console.log('Success: ', result);
+    })
+    .catch(error => {
+        console.log('Error: ', error)
+    })
+}
