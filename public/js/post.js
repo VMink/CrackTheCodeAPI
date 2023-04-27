11edaeb6dda8509f4f7cae106996f49f9efbb821
AldehilS <sanheraldehil@outlook.com>
2023-04-27 16:44:55 -0600

    aoidseif

--- public/js/post.js
+++ public/js/post.js
@@ -1,19 +1,24 @@
 const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
+form.addEventListener('submit',  async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
     console.log(formData)
-    fetch("http://52.55.120.19:8080/register", {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log('Success: ', result);
-    })
-    .catch(error => {
-        console.log('Error: ', error)
-    })
-}
+
+    try {
+        const response = await fetch("http://52.55.120.19:8080/register", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: formData
+        })
+    
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
+});
