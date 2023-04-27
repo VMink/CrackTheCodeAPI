8a37355cd3467553c5f16c8fa75aafb9e45cba06
AldehilS <sanheraldehil@outlook.com>
2023-04-27 16:55:41 -0600

    adoinado

--- api.js
+++ api.js
@@ -37,7 +37,8 @@ connectDb();
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname+'/public'))
 // app.use(express.json());
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.render('index')
