e700192d458631e6403c44d4626e6f19b2a2e705
AldehilS <sanheraldehil@outlook.com>
2023-04-27 20:07:48 -0600

    fpoeskfs

--- public/js/post.js
+++ public/js/post.js
@@ -1,3 +1,6 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
 function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
