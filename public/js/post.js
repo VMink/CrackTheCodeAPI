512dc0dff5f84d2732d3eaab12f4b916f537736b
AldehilS <sanheraldehil@outlook.com>
2023-04-28 10:00:15 -0600

    csoiedcjs
    Co-authored-by: Eduardo Francisco Lugo Quintana <VMink@users.noreply.github.com>

--- public/js/post.js
+++ public/js/post.js
@@ -1,11 +1,18 @@
+const phoneInputField = document.querySelector("#telefono");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+})
+
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
+    const phoneNumber = phoneInput.getNumber();
+    console.log(phoneNumber)
     const formData = new FormData(event.target);
     const jsonData = Object.fromEntries(formData.entries());
-    console.log(jsonData);
     fetch("http://52.55.120.19:8080/register", {
         method: 'POST',
         headers: {
