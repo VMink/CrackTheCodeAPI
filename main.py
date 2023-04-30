c08c41fc140a4962679c548a94ac76ae1dfed903
AldehilS <sanheraldehil@outlook.com>
2023-04-30 11:54:18 -0600

    prueba recolectar datos

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pymssql
-import asyncio
 from datetime import datetime
+
+def get_usuarios():
+    cursor.execute('select * from usuario;')
+    return cursor.fetchall()
+
+def get_partidas():
+    cursor.execute('select * from partida;')
+    return cursor.fetchall()
+
+def get_partidasminijuego():
+    cursor.execute('select * from [partida-minijuego];')
+    return cursor.fetchall()
+
+def get_minijuegos():
+    cursor.execute('select * from minijuego;')
+    return cursor.fetchall()
+
+def get_habilidades():
+    cursor.execute('select * from habilidad;')
+    return cursor.fetchall()
+
 
 connection = pymssql.connect(server='localhost', user='sa', password='7LhDkK$M', database='juego')
 cursor = connection.cursor()
 
-cursor.execute('select count(*) from usuario;')
-count_idUsuario = cursor.fetchone()[0]
+usuarios = get_usuarios()
+partidas = get_partidas()
+partidasminijuego = get_partidasminijuego()
+minijuegos = get_minijuegos()
+habilidades = get_habilidades()
 
-print(count_idUsuario)
-
-# async def get_users():
-#     cursor.execute('select * from usuario')
-#     usuarios = await cursor.fetchall()
-#     return usuarios
-
-# async def get_partidas():
-#     cursor.execute('select * from partida')
-#     partidas = await cursor.fetchall()
-#     return partidas
-
-# async def main():
-
-#     result = asyncio.gather
-#     return 
-
-
-# asyncio.run(main)
+print(usuarios)
+print(partidas)
 
 connection.close()
