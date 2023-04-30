0b80daee8ecbd6d84f970eb3e09ccac6d44df125
AldehilS <sanheraldehil@outlook.com>
2023-04-30 11:38:46 -0600

    espofjse

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import pymssql
-import datetime
+import asyncio
+from datetime import datetime
 
 connection = pymssql.connect(server='localhost', user='sa', password='7LhDkK$M', database='juego')
 cursor = connection.cursor()
 
-cursor.execute('SELECT * FROM usuario;')
-rows = cursor.fetchall()
+cursor.execute('select count(*) from usuario;')
+count_idUsuario = cursor.fetchone()
 
-for row in rows:
-    print(row)
+print(count_idUsuario)
+
+# async def get_users():
+#     cursor.execute('select * from usuario')
+#     usuarios = await cursor.fetchall()
+#     return usuarios
+
+# async def get_partidas():
+#     cursor.execute('select * from partida')
+#     partidas = await cursor.fetchall()
+#     return partidas
+
+# async def main():
+
+#     result = asyncio.gather
+#     return 
+
+
+# asyncio.run(main)
 
 connection.close()
