d5f9576e59e373366eeca9b88fcce44619e823c4
AldehilS <sanheraldehil@outlook.com>
2023-05-01 21:10:31 -0600

    fwfuhewifu

--- public/js/post_admin.js
+++ public/js/post_admin.js
@@ -5,7 +5,7 @@ function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const jsonData = Object.fromEntries(formData.entries());
-    fetch("http://52.55.120.19:8080/register", {
+    fetch("http://52.55.120.19:8080/login-admin", {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
