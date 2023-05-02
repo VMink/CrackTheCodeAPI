a2eb2fa7074d5e62b029c6323fce04e180c5dbeb
AldehilS <sanheraldehil@outlook.com>
2023-05-01 22:02:56 -0600

    sdiojsdi

--- public/js/post_admin.js
+++ public/js/post_admin.js
@@ -1,34 +1,12 @@
 const form = document.getElementById('form');
 form.addEventListener('submit', handleSubmit);
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault(); 
-//     const formData = new FormData(form); 
-//     const url = form.action; 
-//     const queryParams = new URLSearchParams(formData).toString(); 
-//     const fullUrl = `${url}?${queryParams}`; 
-//     window.href = fullUrl;
-//   });
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    const formData = new FormData(form); 
+    const url = form.action; 
+    const queryParams = new URLSearchParams(formData).toString(); 
+    const fullUrl = `${url}?${queryParams}`; 
+    window.href = fullUrl;
+  });
   
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const jsonData = Object.fromEntries(formData.entries());
-    fetch("http://52.55.120.19:8080/login-admin", {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(jsonData)
-    }).then(response => response.json())
-    .then(result => {
-        if (result.error == 'si') {
-            alert('Error: ' + result.resultado);
-        } else {
-            window.location = 'http://52.55.120.19:8080/tableau';
-        }
-    })
-    .catch(error => {
-        console.log('Error: ', error)
-    })
-}
