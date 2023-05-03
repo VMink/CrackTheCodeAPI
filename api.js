dcfccc3e0f121a44846eb9b97298300d34bbfe52
AldehilS <sanheraldehil@outlook.com>
2023-05-03 12:28:39 -0600

    fjesoefji

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const mssql = require('mssql');
 const { createHash } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const compression = require('compression');
 
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.static(__dirname+'/public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(compression({ level: zlib.constants.BROTLI_MAX_QUALITY }));
 
 function hashSHA3_256(data) {
   const hash = createHash('sha3-256');
