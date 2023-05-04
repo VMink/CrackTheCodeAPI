985a5c7c503c342ee521ad0e7869955ea2b89913
AldehilS <sanheraldehil@outlook.com>
2023-05-04 12:15:41 -0600

    doiejd

--- public/js/post_admin.js
+++ public/js/post_admin.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e);
+form.addEventListener('submit', (e) => {
     e.preventDefault(); 
     const formData = new FormData(form); 
     const url = form.action; 
