89107bd2cd37a8c5ef959bcd51e58d857e36c47b
AldehilS <sanheraldehil@outlook.com>
2023-05-04 12:03:51 -0600

    efosief

--- public/js/post.js
+++ public/js/post.js
@@ -5,9 +5,8 @@ const phoneInput = window.intlTelInput(phoneInputField, {
 })
 
 const form = document.getElementById('form');
-form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
+// form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', event => {
     event.preventDefault();
     const phoneNumber = phoneInput.getNumber();
     const formData = new FormData(event.target);
@@ -33,4 +32,32 @@ function handleSubmit(event) {
     .catch(error => {
         console.log('Error: ', error)
     })
-}
+})
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const phoneNumber = phoneInput.getNumber();
+//     const formData = new FormData(event.target);
+//     const jsonData = Object.fromEntries(formData.entries());
+//     jsonData.telefono = phoneNumber;
+//     fetch("http://52.55.120.19:8080/register", {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//           },
+//         body: JSON.stringify(jsonData)
+//     })
+//     .then(response => response.json())
+//     .then(result => {
+//         if (result.error == 'si') {
+//             alert('Error: ' + result.resultado);
+//         } else {
+//             alert('Cuenta registrada!\n' + result.resultado);
+//             const url = form.action;
+//             window.href = url;
+//         }
+//     })
+//     .catch(error => {
+//         console.log('Error: ', error)
+//     })
+// }
